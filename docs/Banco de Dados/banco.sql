863f3a79758a738e84ec8bb563d9a07dbf9ed646
Amanda Silva <adsilva32@gmail.com>
2013-10-29 19:04:51 -0200

    adicionando codigo do projeto

--- docs/Banco de Dados/banco.sql
+++ docs/Banco de Dados/banco.sql
@@ -1,24 +1,27 @@
+drop table usuario;
+drop table documento;
+drop table perdocusuario;
 
 CREATE TABLE `usuario` (
-  `idUsuario` int(11) NOT NULL,
-  `nomeUsuario` varchar(45) NOT NULL,
-  `setorUsuario` varchar(45) NOT NULL,
-  `cargoUsuario` varchar(45) NOT NULL,
-  `emailUsuario` varchar(45) NOT NULL,
-  PRIMARY KEY (`idUsuario`)
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `documento` (
-  `nomeDoc` varchar(45),
-  `idDoc` int(5) NOT NULL,
+  `nome` varchar(45),
+  `id` int(5) NOT NULL,
   `dataCri` varchar(45) DEFAULT NULL,
-  `idUserDoc` int(5) DEFAULT NULL,
-  PRIMARY KEY (`idDoc`)
+  `idUserDoc` int(5) references usuario(`id`),
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `perdocusuario` (
-  `idDoc` int(5) DEFAULT NULL,
-  `idUsuario` int(5) DEFAULT NULL,
+  `idDoc` int(5) DEFAULT NULL REFERENCES documento(`id`),
+  `idUsuario` int(5) DEFAULT NULL REFERENCES usuario(`id`),
   `perLeitura` binary(1) DEFAULT NULL,
   `perEditar` binary(1) DEFAULT NULL,
   `perExcluir` binary(1) DEFAULT NULL
